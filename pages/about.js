c9bbf2702ae1f855c62ab036003d60ef7cac9394
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-01 00:02:34 +0500

    Added slider

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
 import ContentHeader from "../components/common/ContentHeader";
+import DescriptedDotSlider from "../components/common/DescriptedDotSlider";
 
 export default function About() {
     return (
         <div>
-            <ContentHeader class="about" address="about" title="О нас">
+            <ContentHeader class="about" pages={[["about", "О нас"]]}>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo id beatae dolores recusandae
                     et repellat ratione! Culpa accusamus consequatur quae ipsam quidem, reiciendis distinctio
@@ -26,30 +27,11 @@ export default function About() {
                 </p>
             </ContentHeader>
             <div className="about-slider-wrapper content-block">
-                <div className="about-slider-container">
-                    <div className="about-slide">
-                        <img src="/images/about/slider/philippe-oursel-8jFnojODWjc-unsplash.jpg" alt="" width="100%" />
-                    </div>
-                    <div className="dots-container">
-                        <div className="dot active" />
-                        <div className="dot" />
-                        <div className="dot" />
-                        <div className="dot" />
-                        <div className="dot" />
-                    </div>
-                    <div className="alt-text-container">
-                        <p className="alt-p">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere quisquam, quasi distinctio
-                            alias nisi corrupti at ratione, delectus aut minima ducimus iusto repellat sed nam facilis vitae
-                            possimus sapiente vero magni laboriosam officia perferendis. Minus officia libero modi ipsa illo
-                            cupiditate quae nam dolorem porro sint, iusto saepe facere velit. Sequi itaque sint voluptates
-                            minima ipsam quas quidem tempore culpa odio, nulla, ad, accusamus quod asperiores excepturi
-                            repellat laboriosam eveniet laudantium eum! Eius nam, neque consequatur quibusdam officiis
-                            beatae tempora illum vel voluptatum, architecto omnis sequi repudiandae? Nemo porro obcaecati,
-                            quos esse placeat distinctio laboriosam aspernatur exercitationem numquam, aut fuga?
-                        </p>
-                    </div>
-                </div>
+                <DescriptedDotSlider containerClass="about-slider-container" slideClass="about-slide" images={[
+                    ["/images/about/slider/1.jpg", "Описание для фото 1"],
+                    ["/images/about/slider/2.jpg", "Описание для фото 2"],
+                    ["/images/about/slider/3.jpg", "Описание для фото 3"]
+                ]} />
             </div>
         </div>
     );
