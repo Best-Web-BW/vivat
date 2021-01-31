63e45e706d18d7b8e16478afde2ce41af68adabd
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-01 00:01:04 +0500

    Simplifying

--- pages/services.js
+++ pages/services.js
@@ -1,9 +1,35 @@
+import Link from "next/link";
 import ContentHeader from "../components/common/ContentHeader";
+
+function Service(props) {
+    let link = `/services/${props.link}`;
+    return (
+        <div className="services-block">
+            <Link href={link}>
+                <a>
+                    <div className="service-img">
+                        <img src={props.img} alt="" height="100%" />
+                    </div>
+                </a>
+            </Link>
+            <div className="service-category">
+                <Link href={link}>
+                    <a>{props.category}</a>
+                </Link>
+            </div>
+            <div className="service-title">
+                <Link href={link}>
+                    <a>{props.title}</a>
+                </Link>
+            </div>
+        </div>
+    );
+}
 
 export default function Services() {
     return (
         <div>
-            <ContentHeader class="services" address="services" title="Аренда и услуги">
+            <ContentHeader class="services" pages={[["services", "Аренда и услуги"]]}>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo id beatae dolores recusandae
                     et repellat ratione! Culpa accusamus consequatur quae ipsam quidem, reiciendis distinctio
@@ -20,45 +46,24 @@ export default function Services() {
                     <h2>Наши услуги</h2>
                 </div>
                 <div className="services-blocks-wrapper">
-                    <div className="services-block">
-                        <a href="services/main-horsestable.html">
-                            <div className="service-img">
-                                <img src="/images/home/services/evan-wise-O1S18WAol3w-unsplash.jpg" alt="" height="100%" />
-                            </div>
-                        </a>
-                        <div className="service-category">
-                            <a href="!!???">Аренда</a>
-                        </div>
-                        <div className="service-title">
-                            <a href="services/main-horsestable.html">Основная конюшня</a>
-                        </div>
-                    </div>
-                    <div className="services-block">
-                        <a href="services/guest-horsestable.html">
-                            <div className="service-img">
-                                <img src="/images/home/services/gabriella-clare-marino-O_jE3p48m44-unsplash.jpg" alt="" height="100%" />
-                            </div>
-                        </a>
-                        <div className="service-category">
-                            <a href="!!???">Аренда</a>
-                        </div>
-                        <div className="service-title">
-                            <a href="services/guest-horsestable.html">Гостевая конюшня</a>
-                        </div>
-                    </div>
-                    <div className="services-block">
-                        <a href="services/personal-lessons.html">
-                            <div className="service-img">
-                                <img src="/images/home/services/matthew-lancaster-B5KfPvpVe-c-unsplash.jpg" alt="" height="100%" />
-                            </div>
-                        </a>
-                        <div className="service-category">
-                            <a href="!!???">Услуги</a>
-                        </div>
-                        <div className="service-title">
-                            <a href="services/personal-lessons.html">Персональные занятия</a>
-                        </div>
-                    </div>
+                    <Service
+                        img="/images/home/services/evan-wise-O1S18WAol3w-unsplash.jpg"
+                        link="rent-main"
+                        category="Аренда"
+                        title="Основная конюшня"
+                    />
+                    <Service
+                        img="/images/home/services/gabriella-clare-marino-O_jE3p48m44-unsplash.jpg"
+                        link="rent-guest"
+                        category="Аренда"
+                        title="Гостевая конюшня"
+                    />
+                    <Service
+                        img="/images/home/services/matthew-lancaster-B5KfPvpVe-c-unsplash.jpg"
+                        link="personal-lessons"
+                        category="Услуги"
+                        title="Персональные занятия"
+                    />
                 </div>
             </div>
         </div>
