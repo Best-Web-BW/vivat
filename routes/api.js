330e9839d88b0703536d0c0a1727dbb9f1fbdb1f
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-03 22:50:09 +0500

    Changes events plug and added date-filtering

--- routes/api.js
+++ routes/api.js
@@ -1,162 +1,112 @@
 var express = require("express");
 var router = express.Router();
 
+const events = [
+    {
+        id: 1,
+        title: "Мероприятие 1",
+        dates: ["2021-02-01", "2021-02-04"],
+        desc: "Описание мероприятия 1",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 2,
+        title: "Мероприятие 2",
+        dates: ["2021-02-02", "2021-02-03"],
+        desc: "Описание мероприятия 2",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 3,
+        title: "Мероприятие 3",
+        dates: ["2021-02-03", "2021-02-03"],
+        desc: "Описание мероприятия 3",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 4,
+        title: "Мероприятие 4",
+        dates: ["2021-02-04", "2021-02-04"],
+        desc: "Описание мероприятия 4",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 5,
+        title: "Мероприятие 5",
+        dates: ["2021-02-05", "2021-02-06"],
+        desc: "Описание мероприятия 5",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 6,
+        title: "Мероприятие 6",
+        dates: ["2021-02-06", "2021-02-09"],
+        desc: "Описание мероприятия 6",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 7,
+        title: "Мероприятие 7",
+        dates: ["2021-02-07", "2021-02-10"],
+        desc: "Описание мероприятия 7",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 8,
+        title: "Мероприятие 8",
+        dates: ["2021-02-08", "2021-03-02"],
+        desc: "Описание мероприятия 8",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    },
+    {
+        id: 9,
+        title: "Мероприятие 9",
+        dates: ["2021-02-09", "2021-02-12"],
+        desc: "Описание мероприятия 9",
+        place: "Некоторый адрес",
+        category: "Некоторая категория"
+    }
+];
+
 const plugs = {
 	index: "Hello world! index",
     woppa: (woppa) => `Hello world! ${woppa}`,
-    events: JSON.stringify([
-        {
-            id: 1,
-            title: "Мероприятие 1",
-            date: "2021-02-1"
-        },
-        {
-            id: 2,
-            title: "Мероприятие 2",
-            date: "2021-02-2"
-        },
-        {
-            id: 3,
-            title: "Мероприятие 3",
-            date: "2021-02-3"
-        },
-        {
-            id: 4,
-            title: "Мероприятие 4",
-            date: "2021-02-4"
-        },
-        {
-            id: 5,
-            title: "Мероприятие 5",
-            date: "2021-02-5"
-        },
-        {
-            id: 6,
-            title: "Мероприятие 6",
-            date: "2021-02-6"
-        },
-        {
-            id: 7,
-            title: "Мероприятие 7",
-            date: "2021-02-7"
-        },
-        {
-            id: 8,
-            title: "Мероприятие 8",
-            date: "2021-02-8"
-        },
-        {
-            id: 9,
-            title: "Мероприятие 9",
-            date: "2021-02-9"
-        },
-        {
-            id: 10,
-            title: "Мероприятие 10",
-            date: "2021-02-10"
-        },
-        {
-            id: 11,
-            title: "Мероприятие 11",
-            date: "2021-02-11"
-        },
-        {
-            id: 12,
-            title: "Мероприятие 12",
-            date: "2021-02-12"
-        },
-        {
-            id: 13,
-            title: "Мероприятие 13",
-            date: "2021-02-13"
-        },
-        {
-            id: 14,
-            title: "Мероприятие 14",
-            date: "2021-02-14"
-        },
-        {
-            id: 15,
-            title: "Мероприятие 15",
-            date: "2021-02-15"
-        },
-        {
-            id: 16,
-            title: "Мероприятие 16",
-            date: "2021-02-16"
-        },
-        {
-            id: 17,
-            title: "Мероприятие 17",
-            date: "2021-02-17"
-        },
-        {
-            id: 18,
-            title: "Мероприятие 18",
-            date: "2021-02-18"
-        },
-        {
-            id: 19,
-            title: "Мероприятие 19",
-            date: "2021-02-19"
-        },
-        {
-            id: 20,
-            title: "Мероприятие 20",
-            date: "2021-02-20"
-        },
-        {
-            id: 21,
-            title: "Мероприятие 21",
-            date: "2021-02-21"
-        },
-        {
-            id: 22,
-            title: "Мероприятие 22",
-            date: "2021-02-22"
-        },
-        {
-            id: 23,
-            title: "Мероприятие 23",
-            date: "2021-02-23"
-        },
-        {
-            id: 24,
-            title: "Мероприятие 24",
-            date: "2021-02-24"
-        },
-        {
-            id: 25,
-            title: "Мероприятие 25",
-            date: "2021-02-25"
-        },
-        {
-            id: 26,
-            title: "Мероприятие 26",
-            date: "2021-02-26"
-        },
-        {
-            id: 27,
-            title: "Мероприятие 27",
-            date: "2021-02-27"
-        },
-        {
-            id: 28,
-            title: "Мероприятие 28",
-            date: "2021-02-28"
-        }
-    ])
+    events: JSON.stringify(events.map(event => ({ id: event.id, title: event.title, dates: event.dates }))),
+    events_id: (id) => JSON.stringify({ ...events.find(event => event.id == id), full: true})
 };
 
-/* GET home page. */
 router.get("/", (req, res) => {
 	res.end(plugs.index);
 });
 
 router.get("/events", (req, res) => {
-    console.log("Event list requested");
-    res.end(plugs.events);
+    // console.log("Event list requested");
+    const date = req.query.date;
+    if(date) {
+        // console.log("Date", date);
+        const filtered = events.filter(event => event.dates[0] <= date && date <= event.dates[1]);
+        // console.log("Filtered", filtered);
+        const mapped = filtered.map(event => ({ id: event.id, title: event.title, dates: event.dates }));
+        // console.log("Mapped", mapped);
+        const json = JSON.stringify(mapped);
+        // console.log("JSON", json);
+        res.end(json);
+    } else {
+        res.end(plugs.events);
+    }
 })
+
+router.get("/events/:id", (req, res) => {
+    res.end(plugs.events_id(+req.params.id));
+});
 
 router.get("/:woppa", (req, res) => {
 	res.end(plugs.woppa(req.params.woppa));
