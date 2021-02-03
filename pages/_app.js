933633f53b490eb9a3f9489b639c7d32e2bec1cd
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-03 22:51:48 +0500

    Added useful Math functions

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import Layout from "../components/layout";
 
 export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        Math.bound = (num, a, b) => Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
+        Math.cycle = (num, border) => border ? +Math.abs((num >= 0 ? num : num - Math.floor(num / border) * border) % border).toFixed(8) : num;
+    }, []);
+    
 	return (
 		<Layout>
 			<Component { ...pageProps } />
