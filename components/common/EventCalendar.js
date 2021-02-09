71ec121fbeacc40a442a4946e141047542bd3910
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-09 17:59:13 +0500

    Changes

--- components/common/EventCalendar.js
+++ components/common/EventCalendar.js
@@ -2,23 +2,24 @@ import { useState, useEffect } from "react";
 import moment from "moment";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import EventListProvider from "../../utils/providers/EventListProvider";
+import { currentISODate } from "../../utils/common";
 
 export default function EventCalendar() {
-    const events = [
-        {
-            start: moment().toDate(),
-            end: moment().add(1, "days").toDate(),
-            title: "Hello world!"
-        }
-    ];
     const [calendar, setCalendar] = useState(null);
 
     useEffect(async () => {
-        global.ev = () => EventListProvider.getEventList();
         const json = await EventListProvider.getEventList();
-        console.log(json);
-
-        setCalendar(<Calendar localizer={momentLocalizer(moment)} events={events} />);
+        const date = currentISODate(new Date());
+        const events = [];
+        for(let { title, dates: [start, end] } of json) {
+            if(date <= end) events.push({
+                start: moment.utc(start),
+                end: moment.utc(end),
+                title: title,
+            });
+        }
+        setCalendar(<Calendar localizer={momentLocalizer(moment)} events={events} views={["month"]} />
+        );
     }, []);
 
     return calendar;
--- utils/common.js
+++ utils/common.js
@@ -1,2 +1,8 @@
 export const sleep = delay => new Promise(resolve => setTimeout(resolve, delay));
 export const reformatDate = rawDate => new Date(rawDate).toLocaleString("ru", { day: "numeric", month: "numeric", year: "numeric" });
+export const currentISODate = () => {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${date.getFullYear()}-${month < 10 ? "0" : ""}${month}-${day < 10 ? "0" : ""}${day}`;
+}
