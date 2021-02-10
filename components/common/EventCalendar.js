c1310e9803d0d6957d5a0e268e838cadadfe5e50
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-10 15:04:09 +0500

    Update EventCalendar.js

--- components/common/EventCalendar.js
+++ components/common/EventCalendar.js
@@ -1,9 +1,8 @@
-import { useState, useEffect, useMemo } from "react";
-import Router from "next/router";
-import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
+import Router from "next/router";
+import { useState, useEffect, useMemo } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import EventListProvider from "../../utils/providers/EventListProvider";
-import { currentISODate } from "../../utils/common";
 
 export default function EventCalendar() {
     const localizer = useMemo(() => momentLocalizer(moment), []);
@@ -11,15 +10,14 @@ export default function EventCalendar() {
 
     useEffect(async () => {
         const json = await EventListProvider.getEventList();
-        const date = currentISODate(new Date());
 
         const config = {
             localizer,
             views: ["month"],
-            events: json.map(({ id, title, dates: [start, end] }) => ({ id, title, start, end })),
-            startAccessor: event => moment.utc(event.start).toDate(),
             endAccessor: event => moment.utc(event.end).toDate(),
-            onSelectEvent: event => Router.push(`/events/${event.id}`)
+            startAccessor: event => moment.utc(event.start).toDate(),
+            onSelectEvent: event => Router.push(`/events/${event.id}`),
+            events: json.map(({ id, title, dates: [start, end] }) => ({ id, title, start, end }))
         };
 
         setConfig(config);
