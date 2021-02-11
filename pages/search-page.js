1faad7b158abe666f973a24dcb8f108dc5c18297
Mikhail-Litvinov <69105262+Mikhail-Litvinov@users.noreply.github.com>
2021-02-11 18:02:50 +0300

    search-page

---
+++ pages/search-page.js
@@ -0,0 +1,30 @@
+import ContentHeader from "../components/common/ContentHeader";
+
+export default function SearchPage() {
+    return (
+        <div>
+            <ContentHeader class="search" pages={[["about", "О нас"]]}>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo id beatae dolores recusandae
+                    et repellat ratione! Culpa accusamus consequatur quae ipsam quidem, reiciendis distinctio
+                    ratione aut dolore praesentium omnis quis nam modi ea architecto eveniet sunt exercitationem,
+                    totam quas aperiam cupiditate harum vero ex nihil. Aut nisi adipisci amet fugit, aliquid vel
+                    temporibus quos id provident, esse illo explicabo animi inventore at numquam? Accusantium ab
+                    dolor odit repudiandae possimus tempora eveniet autem, reprehenderit voluptatum consectetur nemo
+                    ipsam nesciunt consequuntur sequi fuga odio voluptatem, natus pariatur ullam temporibus sint
+                    rerum consequatur. Quibusdam quod sapiente debitis nulla, ad omnis ratione minima.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo id beatae dolores recusandae
+                    et repellat ratione! Culpa accusamus consequatur quae ipsam quidem, reiciendis distinctio
+                    ratione aut dolore praesentium omnis quis nam modi ea architecto eveniet sunt exercitationem,
+                    totam quas aperiam cupiditate harum vero ex nihil. Aut nisi adipisci amet fugit, aliquid vel
+                    temporibus quos id provident, esse illo explicabo animi inventore at numquam? Accusantium ab
+                    dolor odit repudiandae possimus tempora eveniet autem, reprehenderit voluptatum consectetur nemo
+                    ipsam nesciunt consequuntur sequi fuga odio voluptatem, natus pariatur ullam temporibus sint
+                    rerum consequatur. Quibusdam quod sapiente debitis nulla, ad omnis ratione minima.
+                </p>
+            </ContentHeader>
+        </div>
+    );
+}
