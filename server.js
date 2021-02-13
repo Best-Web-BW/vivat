8258007ea3ce9aa2f6a61e0de31ee1293be7a36d
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-13 16:20:29 +0500

    Minor change

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ const admin = require("./routes/admin.js");
 		server.get("*", (request, response) => handle(request, response));
 
         console.log(`--> Process environment: '${process.env.NODE_ENV}'`);
-        console.log(`--> App environment: '${dev ? "development" : "production"}'`);
+        console.log(`--> Is app in development mode: ${dev}`);
         console.log(`-> Ready on port ${HTTP_PORT} for HTTP`);
         console.log(`-> Ready on port ${HTTPS_PORT} for HTTPS`);
 	} catch(e) {
