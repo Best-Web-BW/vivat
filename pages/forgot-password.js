2ce135a403d0d70de200277b816a303f0f93b473
Mikhail-Litvinov <69105262+Mikhail-Litvinov@users.noreply.github.com>
2021-02-15 16:00:37 +0300

    SMOrc

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -27,18 +27,35 @@ export default function ForgotPassword() {
                     Прежде всего, команда КСК "Виват, Россия!" ценит Вашу безопасность как во время пребывания в комплексе, так и во время пребывания на нашемм веб-сервисе. 
                     Если Вы забыли пароль от Вашей учетной записи, пожалуйста, введите свой адрес электронной почты, который использовался при регистрации, и мы пришлем Вам более подробную инструкцию.
                 </p>
+                <p>
+                    Вы попали на страницу смена пароля для аккаунта на КСК "Виват, Россия!". Пожалуйста, заполните форму ниже и запомните новый пароль. После Вашего подтверждения, этот пароль будет использоваться для входа в профиль.
+                </p>
             </ContentHeader>
             <div className="forgot-password-block content-block">
-                <div className="forgot-password-input-wrapper">
-                    <p>
-                        Введите адрес электронной почты,
-                        <br />
-                        который был указан при регистрации:
-                    </p>
-                    <input ref={emailRef} type="email" name="email" className="forgot-password-input" placeholder="example@gmail.com" />
+                <div className="forgot-password-input-email">
+                    <div className="forgot-password-input-wrapper">
+                        <p>
+                            Введите адрес электронной почты, который был указан при регистрации:
+                        </p>
+                        <input ref={emailRef} type="email" name="email" className="forgot-password-input" placeholder="example@gmail.com" />
+                    </div>
+                    <button className="forgot-password-button" onClick={submit}>Подтвердить</button>
                 </div>
-                <button className="forgot-password-button" onClick={submit}>Подтвердить</button>
-
+                <div className="forgot-password-create-new">
+                    <div className="forgot-password-input-wrapper">
+                        <p>
+                        <span className="required">*</span>&nbsp;Введите новый пароль:
+                        </p>
+                        <input ref={emailRef} type="email" name="email" className="forgot-password-input" placeholder="example@gmail.com" />
+                    </div>
+                    <div className="forgot-password-input-wrapper">
+                        <p>
+                        <span className="required">*</span>&nbsp;Повторите новый пароль:
+                        </p>
+                        <input ref={emailRef} type="email" name="email" className="forgot-password-input" placeholder="example@gmail.com" />
+                    </div>
+                    <button className="forgot-password-button" onClick={submit}>Подтвердить</button>
+                </div>
                 <div className={`modal-error-wrapper ${errorModal && "opened"}`}>
                     <div className="modal-error-content">
                         <p>Произошла ошибка. Обновите страницу и попробуйте позже.</p>
