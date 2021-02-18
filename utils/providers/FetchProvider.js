650425ca47230347fe4aad95eeadd80a6c73e4dd
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-18 15:11:53 +0500

    Returned https for localhost

--- utils/providers/FetchProvider.js
+++ utils/providers/FetchProvider.js
@@ -1,9 +1,9 @@
-import { isDev, isServer } from "../common";
+import { isServer } from "../common";
 
 export default class FetchProvider {
     static async post(_url, data) {
         try {
-            const url = isServer() ? `http${isDev() ? "" : "s"}://localhost${_url}` : _url;
+            const url = isServer() ? `https://localhost${_url}` : _url;
             const response = await fetch(url, {
                 method: "POST",
                 headers: { "Content-Type": "application/json;charset=utf-8" },
