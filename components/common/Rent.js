bdeafc7dc3165f6650889c4fe0773537162d381c
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-23 17:04:17 +0500

    Update Rent.js

--- components/common/Rent.js
+++ components/common/Rent.js
@@ -2,56 +2,40 @@ import { DefaultErrorModal, ErrorModal, SuccessModal } from "./Modals";
 import AuthProvider from "../../utils/providers/AuthProvider";
 import MailProvider from "../../utils/providers/MailProvider";
 import { toRuDate } from "../../utils/common";
-import TimePicker from "react-time-picker";
-// import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
 import Select from 'react-select';
 import DatePicker from "./DatePicker";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const DO_LOG = false;
 
-const hours = [
-    { value: '09', label: '09' },
-    { value: '10', label: '10' },
-    { value: '11', label: '11' },
-    { value: '12', label: '12' },
-    { value: '13', label: '13' },
-    { value: '14', label: '14' },
-    { value: '15', label: '15' },
-    { value: '16', label: '16' },
-    { value: '17', label: '17' },
-    { value: '18', label: '18' },
-    { value: '19', label: '19' },
-    { value: '20', label: '20' },
-    { value: '21', label: '21' }
-]
-
-const minutes = [
-    { value: '00', label: '00' },
-    { value: '10', label: '10' },
-    { value: '20', label: '20' },
-    { value: '30', label: '30' },
-    { value: '40', label: '40' },
-    { value: '50', label: '50' }
-]
-
-// const TimeButton = ({ time, turn, active }) => <button className={`order-service-time-button ${active && "active"}`} onClick={turn}>{ time }</button>;
+const selectValuer = value => ({ value, label: value });
+const hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"].map(selectValuer);
+const minutes = ["00", "10", "20", "30", "40", "50"].map(selectValuer);
 
 export default function Rent({ text, cost, minHours, hoursText }) {
-    const [selectedTime, setSelectedTime] = useState("");
     const [selectedDate, setSelectedDate] = useState(new Date());
+    const [selectedHour, setSelectedHour] = useState(hours[0]);
+    const [selectedMinute, setSelectedMinute] = useState(minutes[0]);
+
+    useEffect(() => console.log(`Selected time: ${selectedHour.value}:${selectedMinute.value}`), [selectedHour, selectedMinute]);
 
     const [successModal, setSuccessModal] = useState(false);
     const [rentTimeErrorModal, setRentTimeErrorModal] = useState(false);
     const [errorModal, setErrorModal] = useState(false);
 
     const submit = async () => {
-        if(!selectedTime.length) return setRentTimeErrorModal(true);
+        // if(!selectedTime.length) return setRentTimeErrorModal(true);
+
+        const hour = selectedHour.value;
+        const minute = selectedMinute.value;
+        console.log(`Submit with selected time: ${hour}:${minute}`);
+        return;
         
+        const email = AuthProvider.userData.email;
+        const phone = AuthProvider.userData.phone;
+        DO_LOG && console.log(email, phone, text, selectedTime, toRuDate(selectedDate));
+
         try {
-            const email = AuthProvider.userData.email;
-            const phone = AuthProvider.userData.phone;
-            DO_LOG && console.log(email, phone, text, selectedTime, toRuDate(selectedDate));
             const result = await MailProvider.sendRentEmail(email, phone, text, selectedTime, toRuDate(selectedDate));
             if(result.success) {
                 setSuccessModal(true);
@@ -91,23 +75,17 @@ export default function Rent({ text, cost, minHours, hoursText }) {
                 </div>
                 <div className="order-service-time-wrapper">
                     <div className="order-service-time-title">Выбор времени</div>
-                    {/* <TimePicker 
-                        disableClock = "true"
-                        maxDetail = "minute"
-                    /> */}
-                    {/* {
-                        ["09:00 - 12:00", "10:00 - 12:00", "13:00 - 15:00", "15:00 - 18:00", "17:00 - 20:00"]
-                            .map(time => <TimeButton key={time} time={time} active={selectedTime === time} turn={() => setSelectedTime(time)} />)
-                    } */}
                     <div className="order-service-time-select">
-                        <Select 
-                                defaultValue={[hours[0]]}
-                                options={hours}
+                        <Select
+                            onChange={setSelectedHour}
+                            value={selectedHour}
+                            options={hours}
                         />
                         :
-                        <Select 
-                                defaultValue={[minutes[0]]}
-                                options={minutes}
+                        <Select
+                            onChange={setSelectedMinute}
+                            value={selectedMinute}
+                            options={minutes}
                         />
                     </div>
                 </div>
