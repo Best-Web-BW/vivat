8448a2e1f309557ed3e52219a405cb52585ddb0d
OcelotJungle <ocelotjungle@yandex.ru>
2021-02-24 14:06:45 +0500

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,13 @@
 import ContentHeader from "../components/common/ContentHeader";
 import { ResultPage } from "../components/common/YandexSearch";
+import Head from "next/head";
 
 export default function Search() {
     return (
         <div>
+            <Head>
+                <link rel="stylesheet" href="/css/yandexSearch.css" />
+            </Head>
             <ContentHeader wrapperClass="search" pages={[["search", "Результаты поиска"]]}>
                 {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo id beatae dolores recusandae
                 et repellat ratione! Culpa accusamus consequatur quae ipsam quidem, reiciendis distinctio
